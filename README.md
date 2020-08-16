43ede02b12155761186adf971670c478b56d3b8c
Meko Ocampo <meko@Mekos-MacBook-Pro.local>
2020-08-15 18:49:28 -0700

    Information about Submit Event

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WorkingWithEventJS
 Trying to Understand How Events Work in JavaScript.
+
+Notes: 
+311. Working with "preventDefault()"
+
+The default action of a form tag is to send information within it to your server. 
+
+312. Understanding "Capturing" & "Bubbling" Phases
---
+++ assets/scripts/events.js
@@ -0,0 +1,50 @@
+/*
+const button = document.querySelector('button');
+
+//button.onclick = function() {} //onclick attribute for html tag has a property equivalent.
+
+const buttonClickHandler = event => {
+    console.log(event);
+}; 
+*/
+/*
+const anotherButtonClickHandler = () => {
+    console.log('This was clicked!');
+}
+
+button.onclick = buttonClickHandler // A disadvantage to this would be you can only have one function per onclick.
+button.onclick = anotherButtonClickHandler // This will overwrite the other function assignment earlier.
+*/
+
+//button.addEventListener(); // Allows you to add multiple event listneers to an element.
+//button.removeEventListener(); // Allows you to remove mutiple event listneers from an element.
+/*
+button.addEventListener('click',buttonClickHandler);
+
+setTimeout(() => {
+    button.removeEventListener('click', buttonClickHandler);
+}, 2000)
+*/
+/*
+If lets say you had a addEventListner but instead of assigning the function to a name
+you live it as anonymous. 
+
+button.addEventListener('click', () => { console.log('Clicked!');
+});
+
+() => { console.log('Clicked!'); } === () => { console.log('Clicked!'); }
+That is not equal, due to the nature of anonymous functions.
+
+307. Removing Event Listners another good example of bind. 
+
+*/
+
+//Look at MDN web docs for more information about events. 
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(event);
+}); //listen for submit event from form element. This however will only briefly show your content in the
+//console window but it will disappear because the data will be sent to the "server" then page will reload.
+
